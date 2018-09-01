3e398b379d8c2554c37c962812c1a1d7ce7d0d49
Lothar Schulz <lothar.sch@gmx.de>
2018-09-02 00:11:02 +0200

    readme and files renamed

--- 01_manual_gradient.py
+++ 01_manual_gradient.py
@@ -13,11 +13,11 @@ housing = fetch_california_housing()
 m, n = housing.data.shape
 housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 
-# MSE starting between 14.1518 going to 0.534
+# MSE starting between ~ 14.1518 going to ~ 0.534
 scaled_housing_data = scaler.fit_transform(housing.data)
 scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data.data]
 
-# MSE starting between 5.83 going to 4.81
+# MSE starting between ~ 5.83 going to ~ 4.81
 # housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
 # scaled_housing_data_plus_bias = scaler.fit_transform(housing_data_plus_bias.data)
 
@@ -43,14 +43,3 @@ with tf.Session() as sess:
             print("Epoch", epoch, "MSE =", mse.eval())
         sess.run(training_op)
     best_theta = theta.eval()
-
-# Epoch 0 MSE = 9.90542
-# Epoch 100 MSE = 0.705636
-# Epoch 200 MSE = 0.54114
-# Epoch 300 MSE = 0.534413
-# Epoch 400 MSE = 0.531796
-# Epoch 500 MSE = 0.52993
-# Epoch 600 MSE = 0.52855
-# Epoch 700 MSE = 0.527523
-# Epoch 800 MSE = 0.526758
-# Epoch 900 MSE = 0.526185
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Gradient Descent for Machine Learning
+
+## Tensorflow and sklearn learn dataset california housing
+
+#### [manual gradient](01_manual_gradient.py)
+#### [optimaized gradients](02_optimized_gradients.py)
+#### [mini batches](03_mini_batch.py)
